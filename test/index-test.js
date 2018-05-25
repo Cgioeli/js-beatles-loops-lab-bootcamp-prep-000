89d7f04402fba603b7bfd49cc3a084a5376ae727
Chris Gioeli <cgioeli@gmail.com>
2018-05-25 17:45:09 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ describe('Beatles Loops', function() {
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
     function theBeatlesPlay(musicians, instruments){
       return []
-      
+      for (let i = 0)
 
     expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
   

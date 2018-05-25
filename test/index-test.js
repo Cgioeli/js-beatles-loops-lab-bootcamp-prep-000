e316a644d2764ab390fc2bed1a80acd4bae3abff
Chris Gioeli <cgioeli@gmail.com>
2018-05-25 17:42:48 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,9 +2,12 @@ describe('Beatles Loops', function() {
   it("returns an array of strings containing what instruments each musician plays", function() {
     const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    function theBeatlesPlay(musicians, instruments){
 
     expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
-  });
+  
+    }
+    });
 
   describe('johnLennonFacts', function(){
     it("returns an array of strings with exclamation points", function() {
